91590525ce3ef1443bd1e1e067e7a5ddd7f37f35
HARSHITA-th18 <thakurharshita655@gmail.com>
2026-09-22 22:47:25 +0530

    Fix service files after merge

--- src/services/menuService.js
+++ src/services/menuService.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-import {
-  collection,
-  addDoc,
-  getDocs,
-  doc,
-  updateDoc,
-  deleteDoc,
-} from "firebase/firestore";
-
-import db from "../firebase/firestore";
-
-// Add menu item
-export const addMenuItem = async (menuItem) => {
-  const docRef = await addDoc(
-    collection(db, "menu"),
-    menuItem
-  );
-
-  return docRef.id;
-};
-
-// Get all menu items
-export const getMenuItems = async () => {
-  const snapshot = await getDocs(
-    collection(db, "menu")
-  );
-
-  return snapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
-};
-
-// Update menu item
-export const updateMenuItem = async (id, data) => {
-  await updateDoc(
-    doc(db, "menu", id),
-    data
-  );
-};
-
-// Delete menu item
-export const deleteMenuItem = async (id) => {
-  await deleteDoc(
-    doc(db, "menu", id)
-  );
-};
-=======
 import { mockMenu } from "../mock/menu"
 
 export function getAllMenuItems() {
@@ -117,4 +68,3 @@ export function deleteMenuItem(itemId) {
 
   return Promise.resolve()
 }
->>>>>>> origin/adminpanel
--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-import {
-  collection,
-  addDoc,
-  getDocs,
-  doc,
-  updateDoc,
-} from "firebase/firestore";
-
-import db from "../firebase/firestore";
-
-// Create order
-export const createOrder = async (order) => {
-  const docRef = await addDoc(
-    collection(db, "orders"),
-    order
-  );
-
-  return docRef.id;
-};
-
-// Get all orders
-export const getOrders = async () => {
-  const snapshot = await getDocs(
-    collection(db, "orders")
-  );
-
-  return snapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
-};
-
-// Update order status
-export const updateOrderStatus = async (
-  orderId,
-  status
-) => {
-  await updateDoc(
-    doc(db, "orders", orderId),
-    {
-      orderStatus: status,
-    }
-  );
-};
-
-// Update payment status
-export const updatePaymentStatus = async (
-  orderId,
-  status
-) => {
-  await updateDoc(
-    doc(db, "orders", orderId),
-    {
-      paymentStatus: status,
-    }
-  );
-};
-=======
 import { mockOrders } from "../mock/orders"
 
 export function getAllOrders() {
@@ -86,4 +27,3 @@ export function updateOrderStatus(orderId, newStatus) {
 
   return Promise.resolve(order)
 }
->>>>>>> origin/adminpanel
